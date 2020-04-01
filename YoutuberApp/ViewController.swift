0245076f232c2e03c8bc3915905098e606187751
YanoRyo <yanoryo7032@gmail.com>
2020-04-01 10:21:01 +0900

    Initial Commit

---
+++ YoutuberApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutuberApp
+//
+//  Created by 矢野涼 on 2020-04-01.
+//  Copyright © 2020 Ryo Yano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
